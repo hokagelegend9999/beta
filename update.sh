8dd6c6b945b3bb2f088e276a78ba819f996e08eb
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2024-06-06 14:33:28 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -27,33 +27,80 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/menu.zip
-    unzip menu.zip
-    chmod +x menu/*
-    mv menu/* /usr/local/sbin
-    rm -rf menu.zip
-    rm -rf update.sh
-    wget -qO- fv-tunnel "https://raw.githubusercontent.com/hokagelegend9999/alpha/main/config/fv-tunnel" 
-    chmod +x fv-tunnel 
-    bash fv-tunnel
-    rm -rf fv-tunnel
-    cd /usr/local/sbin
-    rm -rf menu
-    rm -rf m-sshws
-    rm -rf addssh
-    rm -rf addtr
-    rm -rf addss
-    rm -rf menu-backup
-    rm -rf backup
-    rm -rf kontol
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/menu
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/m-sshws
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/addssh
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/addtr
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/addss
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/menu-backup
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/backup
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/regis
+    cd /usr/bin
+    wget -qO- fv-tunnel "https://raw.githubusercontent.com/hokagelegend9999/beta/main/config/fv-tunnel" 
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/menu
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/m-sshws
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/addssh
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/addtr
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/addss
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/menu-backup
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/backup
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/add-bot-notif
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/addhost
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/addssh
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/addvless
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/addws
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/autokill
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/autoreboot
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/bot
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/bw
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/ceklim
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/cekss
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/cekssh
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/cektr
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/cekvless
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/cekws
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/clearcache
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/clearlog
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/del-bot-notif
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/delexp
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/delss
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/delssh
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/deltr
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/delvless
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/delws
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/fixcert
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/hapus-bot
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/limit-ip-ssh
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/limitspeed
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/lock
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/m-bot
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/mbot-backup
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/mbot-panel
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/member
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/member-ws
+   wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/menu-x
+  wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/m-noob
+    wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/m-ssws
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/m-trial
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/m-trojan
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/m-vless
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/m-vmess
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/prot
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/regis
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/renewss
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/renewtr
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/renewvless
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/reset
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/restart
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/restart-bot
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/restore
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/run
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/sd
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/speedtest
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/stop-bot
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/tendang
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/trial
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/trialss
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/trialtr
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/trialvless
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/trialws
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/tunnel
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/unlock
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/xp
+     wget https://raw.githubusercontent.com/hokagelegend9999/beta/main/menu/z9dtrial
+    
     chmod +x menu
     chmod +x m-sshws
     chmod +x addssh
@@ -65,13 +112,13 @@ res1() {
     
    
 }
-function noobzvpn() {
-wget "${REPO}/noobzvpns.zip"
-unzip noobzvpns.zip
+function hokagevpns() {
+wget "${REPO}/hokagevpn.zip"
+unzip hokagevpn.zip
 bash install.sh
-rm noobzvpns.zip
-systemctl restart noobzvpns
-print_success "NOOBZVPN"
+rm hokagevpn.zip
+systemctl restart hokagevpn
+print_success "HOKAGEVPN"
 }
 netfilter-persistent
 clear
